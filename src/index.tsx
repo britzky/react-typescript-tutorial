4f4623bddf8423d00e2063bfd4ac38bb1e53d1a6
John <johnclapper89@gmail.com>
2023-05-30 13:08:26 -0400

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Dashboard, SignIn } from './components'
 import reportWebVitals from './reportWebVitals';
+import './styles.css'
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './Theme/themes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home title={'Drones Inventory'}/>} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/signin' element={<SignIn />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+
   </React.StrictMode>
 );
 
